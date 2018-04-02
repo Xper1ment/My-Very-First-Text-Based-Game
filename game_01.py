f5a426f3c5c89973c302fce2105f054b686e729e
real-player <31935674+real-player@users.noreply.github.com>
2018-04-02 21:41:44 +0530

    update the game_01.py
    module function should be called

--- game_01.py
+++ game_01.py
@@ -1,7 +1,7 @@
 '''My Very First Text Based Game'''
-import start_adventure
+from start_adventure import start_adventure 
 
 player_name=input("Please Enter Player Name -> ")
 player_resources=[]
 print("You are on a deserted island.You need to get off that island. In Order to do that, you need to gather supplies available on the island. Press the number assosciated with the option in order to acess them")
-start_adventure()
+start_adventure.start_adventure()
